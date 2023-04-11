54148c1d73433aa2e078e3aca4c07247c6611b69
freehostuser <sdivakaran683@gmail.com>
2023-04-11 18:01:41 +0530

    restored

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,54 +1,20 @@
-"use strict"; 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/greeting')
+      .then(response => setMessage(response.data.message))
+      .catch(error => console.error(error));
+  }, []);
+
   return (
     <div className="App">
-      <div className="box">
-        <div className="image">
-        </div>
-      </div>
-      
-      <div className="box">
-        <div className="image2">
-        </div>
-      </div>
-      
-      <div className="box">
-        <div className="image">
-        </div>
-      </div>
-      
-      <div className="box">
-        <div className="image2">
-        </div>
-      </div>
-      
-      <div className="box">
-        <div className="image">
-        </div>
-      </div>
-      
-      <div className="box">
-        <div className="image2">
-        </div>
-      </div>
-      
-      <div className="box">
-        <div className="image">
-        </div>
-      </div>
-      
-      <div className="box">
-        <div className="image2">
-        </div>
-      </div>
-      
-      <div className="box">
-        <div className="image">
-        </div>
-      </div>
-      
+
+      <h1>SERVER    :     {message}</h1>
     </div>
   );
 }
