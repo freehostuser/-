aded5c54e1430733593fc99a513d245d48313e36
freehostuser <sdivakaran683@gmail.com>
2023-04-11 15:44:29 +0530

    new

---
+++ express/express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/api/greeting', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
+  console.log("RUNNING .....");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -13,7 +13,8 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{message}</h1>
+
+      <h1>SERVER    :     {message}</h1>
     </div>
   );
 }
