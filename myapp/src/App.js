620b45e433e3d81dd899a5e005a32f5264270a7b
freehostuser <sdivakaran683@gmail.com>
2023-04-11 18:15:10 +0530

    vanmam

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -15,6 +15,8 @@ function App() {
     <div className="App">
 
       <h1>SERVER    :     {message}</h1>
+      <br/>
+      <img src="https://avatars.githubusercontent.com/u/90057346?s=400&u=d9f29d67c5270e4cd14b9ba4bb6c23088726a92f&v=4"/>
     </div>
   );
 }
